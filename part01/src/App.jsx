82f489886c59cc4c558447e93402f99ea97cf6a6
olukade muzzammil <Adebayo0209@gmail.com>
2021-07-13 01:20:23 +0100

    feat: ongoing part 01

---
+++ part01/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  // const handleClick = () => {
+  //   console.log("clicked");
+  // };
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+      <button onClick={() => setCounter(0)}>Zero</button>
+    </div>
+  );
+};
+
+export default App;
