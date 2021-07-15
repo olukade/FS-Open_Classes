322b43e3b0359c951296ca110bff5a9c58c20cfe
olukade muzzammil <Adebayo0209@gmail.com>
2021-07-16 00:05:01 +0100

    feat: part1 classes cont.

--- part01/src/App.jsx
+++ part01/src/App.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
+import History from './History'
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  // const handleClick = () => {
-  //   console.log("clicked");
-  // };
-
+  // const [counter, setCounter] = useState(0);
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+    const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    }
+    const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    }
+ 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>plus</button>
-      <button onClick={() => setCounter(0)}>Zero</button>
+      {left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      {right}
+      <History allClicks = {allClicks}/>
+  {/* <div>{counter}</div>
+  <button onClick={() => setCounter(counter + 1)}>plus</button>
+  <button onClick={() => setCounter(0)}>Zero</button> */}
     </div>
   );
 };
